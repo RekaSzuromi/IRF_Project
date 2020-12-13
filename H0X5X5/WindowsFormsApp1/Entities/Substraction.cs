0b0f6192fc5cc7e6bf2dcfb23ecda2718b664b81
Your Name <reka.szuromi@sap.com>
2020-12-13 17:45:55 +0100

    Substraction - Operations

---
+++ H0X5X5/WindowsFormsApp1/Entities/Substraction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1.Abstractions;
+
+namespace WindowsFormsApp1.Entities
+{
+    public class Substraction : Operations
+    {
+        public override void DrawImage(Graphics g)
+        {
+            //RECTANGLE
+            // Create rectangle.
+            Rectangle rect = new Rectangle(400, 30, 60, 15);
+
+            // Draw rectangle to screen.
+            g.FillRectangle((new SolidBrush(Color.Green)), rect);
+        }
+    }
+}
