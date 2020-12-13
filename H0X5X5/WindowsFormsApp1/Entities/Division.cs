8853aa87c7532fce8dbccba12bd1cd10aee0a484
Your Name <reka.szuromi@sap.com>
2020-12-13 17:40:06 +0100

    Division  - Operaion

---
+++ H0X5X5/WindowsFormsApp1/Entities/Division.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1.Abstractions;
+
+namespace WindowsFormsApp1.Entities
+{
+    public class Division : Operations
+    {
+        public override void DrawImage(Graphics g)
+        {
+            //ELLIPSE1
+            // Create location and size of ellipse.
+            int x1 = 40;
+            int y1 = 5;
+
+            // Fill ellipse on screen.
+            g.FillEllipse(new SolidBrush(Color.Green), x1, y1, Width, Height);
+
+            //RECTANGLE
+            // Create rectangle.
+            Rectangle rect = new Rectangle(20, 30, 60, 15);
+
+            // Draw rectangle to screen.
+            g.FillRectangle((new SolidBrush(Color.Green)), rect);
+
+            //ELLIPSE2
+            // Create location and size of ellipse.
+            int x2 = 40;
+            int y2 = 50;
+
+            // Fill ellipse on screen.
+            g.FillEllipse(new SolidBrush(Color.Green), x2, y2, Width, Height);
+
+        }
+    }
+}
