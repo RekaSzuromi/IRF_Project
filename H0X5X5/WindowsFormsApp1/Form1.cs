8760942d0bb914f3bac35796e0821325f886cde3
Your Name <reka.szuromi@sap.com>
2020-12-13 17:52:05 +0100

    Chart Correction

--- H0X5X5/WindowsFormsApp1/Form1.cs
+++ H0X5X5/WindowsFormsApp1/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace WindowsFormsApp1
 {
@@ -63,6 +64,26 @@ namespace WindowsFormsApp1
                 }
 
             }
+
+        }
+
+        private void DataOnChart()
+        {
+
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Close";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
 
         }
 
