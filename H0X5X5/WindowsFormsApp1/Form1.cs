cf0641388956d5c6575de5947f119ab730d45683
Your Name <reka.szuromi@sap.com>
2020-12-13 17:29:43 +0100

    Read data and Binding list

--- H0X5X5/WindowsFormsApp1/Form1.cs
+++ H0X5X5/WindowsFormsApp1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,42 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        BindingList<data> Rates = new BindingList<data>();
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void ReadData()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader sr = new StreamReader(ofd.FileName, Encoding.Default))
+                {
+                    sr.ReadLine(); // Remove headers
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+
+                        data d = new data();
+                        d.Date = Convert.ToDateTime(line[0]);
+                        d.Open = Convert.ToDouble(line[1]);
+                        d.High = Convert.ToDouble(line[2]);
+                        d.Low = Convert.ToDouble(line[3]);
+                        d.Close = Convert.ToDouble(line[4]);
+                        d.Volume = Convert.ToInt32(line[6]);
+                        Rates.Add(d);
+                    }
+                }
+
+            }
+
+        }
+
+
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
