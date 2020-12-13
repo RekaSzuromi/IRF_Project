1502ec531c5972dcaf099867260ebe047ed2ad8f
Your Name <reka.szuromi@sap.com>
2020-12-13 17:36:26 +0100

    Abstract Class

---
+++ H0X5X5/WindowsFormsApp1/Abstractions/Operations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1.Abstractions
+{
+    public abstract class Operations : Label
+    {
+        public Operations()
+        {
+            AutoSize = false;
+            Width = 20;
+            Height = 20;
+
+        }
+
+        private void Operations_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        public abstract void DrawImage(Graphics g);
+    }
+}
